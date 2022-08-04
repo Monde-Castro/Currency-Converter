464f3d9b9ee9955b6a106992a2149cb0e5315db4
Monde Castro <45668982+Monde-Castro@users.noreply.github.com>
2022-08-04 16:41:12 +0200

    Adding files via upload

---
+++ Currency Converter/Currency Converter/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCurrencyConverter
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cboCurrency = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(491, 263)
+        Me.Label1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(71, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(351, 93)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Convert " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "South African Rand - ZAR" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Into"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'cboCurrency
+        '
+        Me.cboCurrency.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboCurrency.FormattingEnabled = True
+        Me.cboCurrency.Location = New System.Drawing.Point(107, 151)
+        Me.cboCurrency.Name = "cboCurrency"
+        Me.cboCurrency.Size = New System.Drawing.Size(282, 32)
+        Me.cboCurrency.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(215, 195)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 24)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Amount"
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAmount.Location = New System.Drawing.Point(181, 222)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(143, 29)
+        Me.txtAmount.TabIndex = 4
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.lblDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDisplay.Location = New System.Drawing.Point(41, 293)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(434, 85)
+        Me.lblDisplay.TabIndex = 5
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.Location = New System.Drawing.Point(59, 408)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(89, 32)
+        Me.btnConvert.TabIndex = 6
+        Me.btnConvert.Text = "C&onvert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(202, 408)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(89, 32)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(333, 408)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(89, 32)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmCurrencyConverter
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(524, 470)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.cboCurrency)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmCurrencyConverter"
+        Me.Text = "Currency Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents cboCurrency As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtAmount As System.Windows.Forms.TextBox
+    Friend WithEvents lblDisplay As System.Windows.Forms.Label
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Currency Converter/Currency Converter/Form1.vb
@@ -0,0 +1,108 @@
+﻿Public Class frmCurrencyConverter
+
+   
+    Private Sub frmCurrencyConverter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Add Items to the combo box
+        'Select the first item in the combo box
+        cboCurrency.Items.Add("Choose a Currency")
+        cboCurrency.Items.Add("United kingdom Pound - GBP")
+        cboCurrency.Items.Add("Euro - EUR")
+        cboCurrency.Items.Add("Russian Rubles - RUB")
+
+    End Sub
+
+    Private Sub cboCurrency_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboCurrency.SelectedIndexChanged
+        'Set properties for amount textbox when value in the currency combo box
+        lblDisplay.Text = " "
+        txtAmount.Focus()
+    End Sub
+
+    Private Function DoConversion(ByVal decDAmount As Decimal,
+                                   ByVal decDExchangeRate As Decimal) As Decimal
+        'Convert to desird currency
+        Dim decConvertedAmount As Decimal
+        decConvertedAmount = decDAmount * decDExchangeRate
+
+        Return decConvertedAmount
+    End Function
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        'Calculate and Display the converted Amount
+        Dim decExchangeRate As Decimal = 0
+        Dim decAmount As Decimal = 0
+        Dim decConvertedAmount As Decimal = 0
+        Dim strCurrency As String
+
+        'Check that curreny is selected in the combo box
+        If cboCurrency.SelectedIndex = 0 Then
+            MessageBox.Show("Select a currency to convert to", " Invalid currency")
+            cboCurrency.DroppedDown = True
+            Exit Sub
+        End If
+
+        'Assign exchange rate to decimal variable
+        'Assign GBP, EUR or RUB to a string variable
+        If cboCurrency.SelectedIndex = 1 Then
+            decExchangeRate = 0.0874832D
+            strCurrency = "GBP"
+        End If
+        If cboCurrency.SelectedIndex = 2 Then
+            decExchangeRate = 0.105135D
+            strCurrency = "EUR"
+        End If
+        If cboCurrency.SelectedIndex = 3 Then
+            decExchangeRate = 4.15483D
+            strCurrency = "RUB"
+        End If
+
+        'Check that the Amount TextBox isn't Empty
+        If txtAmount.Text = " " Then
+            MessageBox.Show("Enter an amount to convert", " Entry error")
+            txtAmount.Clear()
+            txtAmount.Focus()
+            Exit Sub
+        End If
+
+        'Test for valid numeric input in the amount textbox
+        'Convert and assign value to decimal variable
+        If IsNumeric(txtAmount.Text) Then
+            decAmount = CDec(txtAmount.Text)
+        Else
+            MessageBox.Show("Enter a valid amount to convert", " Entry error")
+            txtAmount.Clear()
+            txtAmount.Focus()
+            Exit Sub
+        End If
+
+        'Amount entered must be greater than 0
+        If Not (decAmount > 0) Then
+            MessageBox.Show("Enter a positive Amount", " Entry error")
+            txtAmount.Focus()
+            txtAmount.Clear()
+            Exit Sub
+        End If
+
+        'Assing converterd amount to decimal variable
+        'Call function to do conversion
+        decConvertedAmount = DoConversion(decAmount, decExchangeRate)
+
+
+        'Display output in label
+        lblDisplay.Text = decAmount.ToString("N2") & " ZAR = " & " " &
+            decConvertedAmount.ToString("N2") & strCurrency
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear all controls and set properties
+        cboCurrency.SelectedIndex = 1
+        txtAmount.Clear()
+        lblDisplay.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Terminate program
+        Me.Close()
+
+    End Sub
+End Class
